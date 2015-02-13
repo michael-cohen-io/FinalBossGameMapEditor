dda48511937576b3134c6d6af09b1d892dbc849c
Chris Moscoso <christophermoscoso@gmail.com>
2015-02-13 15:31:38 -0500

    Go to town on the tile class

--- FinalBossMapEditor/src/main/Tile.java
+++ FinalBossMapEditor/src/main/Tile.java
@@ -1,9 +1,34 @@
 package main;
 
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author ChrisMoscoso
  */
 public class Tile {
     private int id;
+    
+    
+    public Tile(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+    
+    private BufferedImage image;
 }
