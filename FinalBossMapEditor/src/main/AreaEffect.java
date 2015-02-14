bbff29a84b5456029f54faff30259703606bcb1d
hahmed1 <hanif314@gmail.com>
2015-02-14 16:18:09 -0500

    other stuff

---
+++ FinalBossMapEditor/src/main/AreaEffect.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum AreaEffect {
+	DEFAULT , TAKEDAMAGE , HEAL, INSTANTDEATH, LEVELUP;
+}
---
+++ FinalBossMapEditor/src/main/Terrain.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum Terrain {
+	DEFAULT , GRASS, WATER, MOUNTAIN
+}
